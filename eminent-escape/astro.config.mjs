aa7fc5014831549a42402538d9a2264428865364
Luis Gerardo Marquez Mendez <lgerardomarquezm@gmail.com>
2025-07-21 17:52:16 -0600

    Listo para desplegar

--- eminent-escape/astro.config.mjs
+++ eminent-escape/astro.config.mjs
@@ -1,8 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailWind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailWind()]
+    integrations: [tailWind()],
+    output: 'static',
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
 });
