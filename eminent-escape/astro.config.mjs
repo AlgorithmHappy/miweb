8647787c54a5c1c6ae9e0dc565f4f213b89ae291
Luis Gerardo Marquez Mendez <lgerardomarquezm@gmail.com>
2026-03-21 20:26:31 -0600

    Se agrega el diseño que me dio cloude, se hacen correcciones al menu en astro, se agrega como componente el nuevo menu, se borra tailwindo, se va a crear un nuevo layout

--- eminent-escape/astro.config.mjs
+++ eminent-escape/astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailWind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailWind()],
+    integrations: [],
     output: 'static',
     adapter: vercel({
         webAnalytics: {
